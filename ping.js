26345a0f54111e33c58b1a95ead3185d17254b37
criphood <criphood4life@gmail.com>
2022-05-27 16:08:06 +0300

    last commit

--- ping.js
+++ ping.js
@@ -11,12 +11,7 @@ let statuses = document.querySelectorAll('.status');
     }
     else { root.ping = factory(); }
 }(this, function () {
-    /**
-     * Creates and loads an image element by url.
-     * @param  {String} url
-     * @return {Promise} promise that resolves to an image element or
-     *                   fails to an Error.
-     */
+
     function request_image(url) {
         return new Promise(function (resolve, reject) {
             var img = new Image();
@@ -25,13 +20,6 @@ let statuses = document.querySelectorAll('.status');
             img.src = url + '?random-no-cache=' + Math.floor((1 + Math.random()) * 0x10000).toString(16);
         });
     }
-
-    /**
-     * Pings a url.
-     * @param  {String} url
-     * @param  {Number} multiplier - optional, factor to adjust the ping by.  0.3 works well for HTTP servers.
-     * @return {Promise} promise that resolves to a ping (ms, float).
-     */
 
     function ping(url, multiplier) {
         return new Promise(function (resolve, reject) {
@@ -43,8 +31,6 @@ let statuses = document.querySelectorAll('.status');
 
             };
             request_image(url).then(response).catch(response);
-
-            // Set a timeout for max-pings, 5s.
         
             setInterval(function () { reject(Error('Timeout: ' + url)); }, 5000);
         });
@@ -53,7 +39,6 @@ let statuses = document.querySelectorAll('.status');
 
     return ping;
 }));
-
 
 
 for (let status of statuses) {
